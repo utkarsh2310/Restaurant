b0da1dd509e09f2d47b43d18d0be8560f0e1939b
Utkarsh mishra <rishumisra2310@gmail.com>
2020-05-03 23:27:36 +0530

    reviews added

---
+++ app/src/main/java/com/utkarsh/restaurant/ViewModel/ReviewViewModel.java
@@ -0,0 +1,4 @@
+package com.utkarsh.restaurant.ViewModel;
+
+public class reviewViewModel {
+}
---
+++ app/src/main/java/com/utkarsh/restaurant/ui/ResReviews.java
@@ -0,0 +1,16 @@
+package com.utkarsh.restaurant.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.utkarsh.restaurant.R;
+
+public class Reviews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviews);
+    }
+}
